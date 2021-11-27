94e798a12920f29af9bbd3d768ff31439bba3215
JMJ Riyana <57560640+Riyana6@users.noreply.github.com>
2021-11-27 10:30:33 +0530

    Added user registration

---
+++ src/main/java/com/spring/mongodb/SpringBootSecurityJwtMongodbApplication.java
@@ -0,0 +1,22 @@
+package com.spring.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/auth/logFile")
+@ComponentScan("com") //to scan packages mentioned
+@EnableMongoRepositories("com") //to activate MongoDB repositories
+@EnableScheduling
+public class SpringBootSecurityJwtMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecurityJwtMongodbApplication.class, args);
+    }
+}
